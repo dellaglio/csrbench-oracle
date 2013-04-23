c3af467679dbab2ae876bc0ed87565da3a5c101e
dellaglio <dani.dellaglio@gmail.com>
2013-04-23 13:41:54 +0200

    Rewritten the Windower test class (now it is a suite with 2 classes: one for windowclose=true and one for contentchange=true)

---
+++ src/test/java/it/polimi/deib/streams/oracle/WindowerTestSuite.java
@@ -0,0 +1,12 @@
+package it.polimi.deib.streams.oracle;
+
+import junit.framework.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({WindowerWindowCloseTest.class, WindowerContentChangeTest.class})
+public class WindowerTestSuite extends TestSuite{
+}
---
+++ src/test/java/it/polimi/deib/streams/oracle/WindowerWindowCloseTest.java
@@ -0,0 +1,73 @@
+package it.polimi.deib.streams.oracle;
+
+import it.polimi.deib.streams.oracle.s2r.ReportPolicy;
+import it.polimi.deib.streams.oracle.s2r.WindowScope;
+import it.polimi.deib.streams.oracle.s2r.Window;
+import it.polimi.deib.streams.oracle.s2r.Windower;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+public class WindowerWindowCloseTest {
+	private static final Logger logger = LoggerFactory.getLogger(WindowerWindowCloseTest.class);
+
+	//W(10,3), t0=2
+	@Test public void timeSlidingWindow2ShouldSlideWithWindowClose(){
+		ReportPolicy policy = new ReportPolicy();
+		policy.setWindowClose(true);
+		policy.setContentChange(false);
+
+		Windower windower = new Windower(new Window(10000,3000), policy, 2000);
+		WindowScope range = windower.getNextWindowScope(null); 
+		assertEquals(2000, range.getFrom());
+		assertEquals(12000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(5000, range.getFrom());
+		assertEquals(15000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(8000, range.getFrom());
+		assertEquals(18000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(11000, range.getFrom());
+		assertEquals(21000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(14000, range.getFrom());
+		assertEquals(24000, range.getTo());
+	}
+
+	//W(10,3), t0=0
+	@Test public void timeSlidingWindow1ShouldSlideWithWindowClose(){
+		ReportPolicy policy = new ReportPolicy();
+		policy.setWindowClose(true);
+		policy.setContentChange(false);
+
+		Windower windower = new Windower(new Window(10000,3000), policy, 0);
+		WindowScope range = windower.getNextWindowScope(null);
+		assertEquals(0, range.getFrom());
+		assertEquals(10000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(3000, range.getFrom());
+		assertEquals(13000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(6000, range.getFrom());
+		assertEquals(16000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(9000, range.getFrom());
+		assertEquals(19000, range.getTo());
+
+		range = windower.getNextWindowScope(null);
+		assertEquals(12000, range.getFrom());
+		assertEquals(22000, range.getTo());
+	}
+
+}
