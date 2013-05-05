e1ebb0fd0f8f3545eda0c1043464bcbcd5d9fb44
dellaglio <dani.dellaglio@gmail.com>
2013-05-05 15:23:18 +0200

    replaced system.out with logger

--- src/main/java/it/polimi/deib/streams/oracle/s2r/Windower.java
+++ src/main/java/it/polimi/deib/streams/oracle/s2r/Windower.java
@@ -8,6 +8,8 @@ import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import it.polimi.deib.streams.oracle.Config;
 import it.polimi.deib.streams.oracle.query.WindowDefinition;
@@ -19,6 +21,8 @@ public class Windower {
 	private ReportPolicy policy;
 	private final long t0;
 	private long actualTime;
+	
+	private static final Logger logger = LoggerFactory.getLogger(Windower.class);
 	
 	public Windower(WindowDefinition window, ReportPolicy policy, long t0) {
 		super();
@@ -55,7 +59,6 @@ public class Windower {
 			if(nextDataTimestamp!=null){
 				actualTime=nextDataTimestamp;
 				while((a*window.getSlide())+t0+window.getSize() <= actualTime){a++;};
-				System.out.println(a+ " " + actualTime+ " " + nextDataTimestamp+ " " + t0);
 				return new WindowScope(Math.max(t0, t0+a*window.getSlide()), actualTime+Config.getInstance().getTimeUnit());
 			}else{
 				return null;
@@ -70,13 +73,13 @@ public class Windower {
 		try {
 			TupleQuery tq = conn.prepareTupleQuery(QueryLanguage.SPARQL, "SELECT ?timestamp FROM <"+BenchmarkVocab.graphList+"> WHERE {?g <"+BenchmarkVocab.hasTimestamp+"> ?timestamp. FILTER(?timestamp > \""+actualTime+"\"^^<http://www.w3.org/2001/XMLSchema#long>)} ORDER BY ?timestamp LIMIT 1");
 			TupleQueryResult tqr = tq.evaluate();
-			System.out.println("SELECT ?timestamp FROM <"+BenchmarkVocab.graphList+"> WHERE {?g <"+BenchmarkVocab.hasTimestamp+"> ?timestamp. FILTER(?timestamp > \""+actualTime+"\"^^<http://www.w3.org/2001/XMLSchema#long>)} ORDER BY ?timestamp LIMIT 1");
+			logger.debug("SELECT ?timestamp FROM <"+BenchmarkVocab.graphList+"> WHERE {?g <"+BenchmarkVocab.hasTimestamp+"> ?timestamp. FILTER(?timestamp > \""+actualTime+"\"^^<http://www.w3.org/2001/XMLSchema#long>)} ORDER BY ?timestamp LIMIT 1");
 			
 			if(tqr.hasNext()){
 				ret = ((Literal)tqr.next().getBinding("timestamp").getValue()).longValue();
 			}
 			else{
-				System.out.println("empty!");
+				logger.debug("the query returned an empty result!");
 			}
 			tqr.close();
 		} catch (RepositoryException e) {
@@ -91,18 +94,5 @@ public class Windower {
 		}
 		return ret;
 	}
-	
-//	public static void main(String[] args) {
-//		int a = 0;
-//		int slide = 5000;
-//		int width = 10000;
-//		int t0=0;
-//		long nextDataTimestamp = 15000;
-//		long actualTime=nextDataTimestamp;
-//		while((a+1)*slide+t0-width  <= actualTime){
-//			a++;
-//		}
-//		System.out.println("["+(a*slide+t0-width) + ", " + (actualTime+Config.getInstance().getTimeUnit()) + ")");
-//	}
 	
 }
